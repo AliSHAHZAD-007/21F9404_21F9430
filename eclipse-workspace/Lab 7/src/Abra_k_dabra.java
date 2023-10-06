1c79672eca2ac4145452f35009271e98a29167ae
unknown <f219430@NUCFD.EDU.LOCAL>
2023-10-06 10:41:46 +0500

    divide function added

--- eclipse-workspace/Lab 7/src/Abra_k_dabra.java
+++ eclipse-workspace/Lab 7/src/Abra_k_dabra.java
@@ -1,10 +1,14 @@
 
 public class Abra_k_dabra {
+	public double divide(double num1, double num2) {
+		return num1/num2;
+	}
 
 	public static void main(String[] args) {
 		Calculator cal_obj = new Calculator();
-		obj.add(5,5);
-		obj.subtract(6,5)
+		obj.add(5.00,5.00);
+		obj.subtract(6.00,5.00);
+		divide(25.00,5.00);
 	}
 
 }
