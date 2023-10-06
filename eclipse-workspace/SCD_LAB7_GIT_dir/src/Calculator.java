25002b3c3230ec79992497b247f9bbac0361669f
Ali Shahzad <alishahzad.as270@gmail.com>
2023-10-06 09:59:08 +0500

    CREATED CALCULATOR CLASS WITH ADD SUBTRACT FUNCTIONS

---
+++ eclipse-workspace/SCD_LAB7_GIT_dir/src/Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+	public double add(double a, double b) {
+		return a + b;
+	}
+	public double subtract(double a, double b) {
+		return a - b;
+	}
+
+}
